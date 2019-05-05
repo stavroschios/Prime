007344a5f282f31aa3c607dae7ce6931b5d7924a
Stavros Pachoundakis <15883219+stavroschios@users.noreply.github.com>
2019-05-05 15:35:23 -0400

    Update AllPrimes.java

--- src/AllPrimes.java
+++ src/AllPrimes.java
@@ -124,7 +124,7 @@ public class AllPrimes {
 
         }
 
-        for (int i=2; i <n; i++) {
+        for (int i=2; i < n/2; i++) {
             if(n%i == 0) {
                 return false;
             }
